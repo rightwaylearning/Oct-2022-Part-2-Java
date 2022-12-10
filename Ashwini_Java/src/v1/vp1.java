50c7569fab604e7d32c010d2e23fd30c8a83df34
AshDecode <ashwinidewade23@gmail.com>
2022-12-10 14:37:02 +0530

    as

---
+++ Ashwini_Java/src/v1/vp1.java
@@ -0,0 +1,20 @@
+package v1;
+
+public class vp1 {
+	
+	public static void main(String[] args) {
+
+	      //int i=1;
+		for (int n = 5; n >= 1; n--) {
+
+			for (int j = 1; j <= n; j++) {
+
+				System.out.print("* "); // n,j
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
