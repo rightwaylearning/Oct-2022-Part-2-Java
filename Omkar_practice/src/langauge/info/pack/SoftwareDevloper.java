29a974be729f201e7bc280290205c219912fc5fc
omkar pawar <pawaromkar885@gmail.com>
2022-12-18 09:54:30 +0530

    toString

---
+++ Omkar_practice/src/langauge/info/pack/SoftwareDevloper.java
@@ -0,0 +1,35 @@
+package langauge.info.pack;
+
+class QAEngg{
+	
+	int a;
+	int b;
+	
+	QAEngg(int a,int b){
+		this.a = a;
+		this.b = b;
+	}
+	@Override
+	public int hashCode() {
+		return a + b;
+	}
+	@Override
+	public String toString() {
+		return "hii hello";
+	}
+}
+public class SoftwareDevloper {
+
+	public static void main(String[] args) {
+		
+		QAEngg q = new QAEngg(1,2);
+		
+		System.out.println(q);  // q.toString() >>> Object
+		System.out.println(q.toString());
+		
+		System.out.println("----------------------");
+		
+		System.out.println(q.getClass().getName());
+		System.out.println(Integer.toHexString(q.hashCode()));
+	}
+}
---
+++ Omkar_practice/src/langauge/info/pack/ToStringDemo.java
@@ -0,0 +1,32 @@
+package langauge.info.pack;
+
+import language.info.pack.PP;
+import language.info.pack.String;
+
+class PP{
+	int a;
+	int b;
+	
+	public PP(int a,int b) {
+		this.a =a;
+		this.b =b;
+	}
+
+	@Override
+	public String toString() {
+		return "PP [a=" + a + ", b=" + b + "]";
+	}
+	
+}
+public class ToStringDemo {
+public static void main(String[] args) {
+		
+		String s = new String("test");
+		PP p = new PP(10,20);
+		
+		System.out.println(s.toString());
+		System.out.println(p.toString());
+		
+	}
+}
+
